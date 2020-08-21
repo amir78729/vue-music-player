faaf45fb696b874309a373c603a92f6305068947
amir78729 <amir78729@gmail.com>
2020-08-21 13:20:07 +0430

    footer song-info

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,12 +5,24 @@
     <div class="slidecontainer">
       <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
     </div>
-    <div class="buttons container align-content-center" align="center">
-      <button class="btn btn-info" id="shuffle"><i class="fa fa-play" aria-hidden="true"></i></button>
-      <button class="btn btn-info" id="prev"><i class="fa fa-play" aria-hidden="true"></i></button>
-      <button class="btn btn-info" id="play"><i class="fa fa-play" aria-hidden="true"></i></button>
-      <button class="btn btn-info" id="next"><i class="fa fa-play" aria-hidden="true"></i></button>
-      <button class="btn btn-info" id="repeat"><i class="fa fa-play" aria-hidden="true"></i></button>
+    <div class="container-fluid">
+      <div class="song-info col-lg-4 row">
+        <div style="" class="song-cover  "></div>
+        <div class="text">
+          <p style="font-size: 18px">{{ currentSongName }}</p>
+          <p style="font-size: 16px">{{ currentSongArtist }}</p>
+        </div>
+      </div>
+      <div class="buttons container align-content-center col-lg-4" align="center">
+        <button class="btn" id="shuffle"><i class="fa fa-play" aria-hidden="true"></i></button>
+        <button class="btn" id="prev"><i class="fa fa-play" aria-hidden="true"></i></button>
+        <button class="btn" id="play"><i class="fa fa-play" aria-hidden="true"></i></button>
+        <button class="btn" id="next"><i class="fa fa-play" aria-hidden="true"></i></button>
+        <button class="btn" id="repeat"><i class="fa fa-play" aria-hidden="true"></i></button>
+      </div>
+      <div class="col-lg-4">
+
+      </div>
     </div>
   </div>
 </template>
@@ -23,6 +35,8 @@
   export default {
     data() {
       return {
+        currentSongName: 'Song Name',
+        currentSongArtist: 'Artist Name',
         timelinePercentage: 0.5
       }
     },
@@ -45,6 +59,9 @@
     align-content: center;
   }
   .buttons button {
+    background-color: #444444;
+    color: #5bc0de;
+    border: solid #5bc0de 2px;
     align-content: center;
     font-size: 18px;
     margin: 4px;
@@ -54,6 +71,9 @@
     transition: all .3s;
   }
   .buttons button:hover {
+    background-color: #333333;
+    color: #5bc0de;
+    border: solid #5bc0de 2px;
     font-size: 20px;
     margin: 2px;
     height: 54px;
@@ -104,5 +124,26 @@
     height: 7px; /* Slider handle height */
     background: red; /* Green background */
     cursor: pointer; /* Cursor on hover */
+  }
+
+  .song-info {
+    float: left;
+    text-space: 10px;
+  }
+  .song-info p {
+    /*font-weight: bold;*/
+    color: #5bc0de;
+    margin-bottom: 0;
+  }
+  .song-info .text {
+    padding: 2px;
+  }
+  .song-cover {
+    background-color: #5bc0de;
+    height: 50px;
+    width: 50px;
+    margin: 7px 5px 5px 0px;
+    position: relative;
+    border-radius: 50%;
   }
 </style>
