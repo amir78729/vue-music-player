554ed06d15a127c628945906011576e539ddc989
amir78729 <amir78729@gmail.com>
2020-08-21 16:48:17 +0430

    search area created

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="bg-warning">
-    <h1>hi</h1>
+  <div class="header">
+    <div class="search">
+      <button class="btn" id="search-button">
+        <i class="fa fa-search" style="color: white; text-align: left"></i>
+      </button>
+      <input type="text" id="search-area">
+
+      </input>
+    </div>
   </div>
 </template>
 
@@ -17,5 +24,61 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  input {
+    border: none;
+    box-shadow: none;
+  }
+  .header{
+    background-color: #333333;
+    height: 60px;
+  }
+  .search{
+    position: relative;
 
+    /*transition: all .5s;*/
+  }
+  .search #search-area {
+    position: absolute;
+    background-color: #ffffff;
+    height: 30px;
+    left: 30px;
+    top: 15px;
+    width: 0;
+    border-radius: 15px;
+    transition: all .5s;
+
+
+  }
+  .search:hover #search-area {
+    width: 100px;
+    left: 15px;
+    transition: all .5s ease-in;
+    /*transition: width .5s;*/
+  }
+  .search #search-button {
+    position: absolute;
+    left: 15px;
+    top: 15px;
+    font-size: 10px;
+    text-align: center;
+    background-color: #5bc0de;
+    width: 30px;
+    height: 30px;
+    border-radius: 15px;
+    z-index: 6;
+    transition: all .5s;
+
+  }
+  .search:hover #search-button {
+    position: absolute;
+    left: 85px;
+    height: 28px;
+    width: 28px;
+    margin: 1px;
+    /*transition: all .5s;*/
+    transition: all .485s ease-in-out;
+    transition-delay:  .15s;
+
+
+  }
 </style>
