7885759426c76a96ca2091907d2fa5eb7b72aefe
amir78729 <amir78729@gmail.com>
2020-09-03 22:15:48 +0430

    friends updated

--- src/friends.js
+++ src/friends.js
@@ -2,23 +2,37 @@ export var friends = [
   {
     name: 'hamid nemati',
     image: 'https://cdn.discordapp.com/attachments/732234196487241741/751000886549151744/20200903_131849.jpg',
+    isOnline: false,
+    currentListening: 'Pounez',
   },{
     name: 'amirhossein',
     image: 'https://cdn.discordapp.com/attachments/732234196487241741/751001986337472613/20200903_132322.jpg',
+    isOnline: false,
+    currentListening: 'Windowpane',
   },{
     name: 'neginhsobhani',
     image: 'https://cdn.discordapp.com/attachments/732234196487241741/751001268637663302/20200903_132038.jpg',
+    isOnline: false,
+    currentListening: 'Still Loving you',
   },{
     name: 'mhassan1378',
     image: 'https://cdn.discordapp.com/attachments/732234196487241741/751019116835504148/20200903_143127.jpg',
+    isOnline: false,
+    currentListening: 'Global Warming',
   },{
     name: 'mohammad_mr8',
     image: 'https://cdn.discordapp.com/attachments/732234196487241741/751034502305808385/IMG_20200903_153248_894.jpg',
+    isOnline: false,
+    currentListening: 'Matarsak',
   },{
     name: 'Rozhin Sattarpour',
     image: 'https://cdn.discordapp.com/attachments/732234196487241741/751035755731877938/20200903_153735.jpg',
+    isOnline: false,
+    currentListening: 'some bullshit',
   },{
     name: 'elem99',
     image: 'https://cdn.discordapp.com/attachments/732234196487241741/751080769581219920/unknown.png',
+    isOnline: false,
+    currentListening: 'How you like that',
   },
 ];
